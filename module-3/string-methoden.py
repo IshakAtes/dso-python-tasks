ee72560b84c41567c67c4393f142e1084b04552b
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2025-01-07 13:00:31 +0100

    string reverse with slicing

--- module-3/string-methoden.py
+++ module-3/string-methoden.py
@@ -46,14 +46,18 @@ print("     Is  h  ak      ".replace(" ", ""))
 # Prüft wie oft etwas vorkommt
 print("Wasserfall, Abfall".count('fall'))
 
-# Prüft an welcher stelle etwas ist, was wir suchen bzw. was wir als parameter übergeben haben
+# Prüft an welcher stelle etwas ist, was wir suchen bzw. was wir als parameter übergeben haben und gibt uns den index zurück
 # Problematik: Wirft ein fehler, wenn nichts gefunden wird
 print("Wasserfall, Abfall".index('s'))
 
 # Sucht an welcher stelle unser parameter vorkommt
-# Problematik: Wirft kein fehler, wenn nichts gefunden wird aber gibt stattdesen -1 zurück
+# Problematik: Wirft kein fehler, wenn nichts gefunden wird aber gibt stattdesen -1 zurück, wenn nichts gefunden wurde
 print("Wasserfall, Abfall".find('f'))
 
 # Gibt ein Wahrheitswert zurück. Ist unser parameter enthalten oder nicht.
 # In unserem Beispiel ist a in Wasserfall enthalten oder nicht. Wenn ja dann bekommen wir True als boolean zurück, sonst false
 print("a" in "Wasserfall")
+
+# [::-1] Dies ist keine Methode sondern eine slicing variante, die sehr häufig verwendet wird,
+# um den String zu verdrehen bzw. zu reversen (9876543210), deshalb steht es hier in der Liste
+print("0123456789"[::-1])
