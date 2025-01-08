baf4ee0a3c47c65b98a59d8fe5c8629670875439
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2025-01-08 13:34:40 +0100

    added some functions in Python

---
+++ module-3/functions-in-python.py
@@ -0,0 +1,36 @@
+# if-elif-else-Anweisungen
+note = input("Gib die Note ein: ")
+
+if note == "1":
+    print("Sehr gut!")
+elif note == "2":
+    print("Gut!")
+elif note == "3":
+    print("Befriedigend.")
+elif note == "4":
+    print("Ausreichend.")
+elif note == "5":
+    print("Mangelhaft!")
+else:
+    print("Ungenügend!!!")
+
+# --------------------------------------------------------------------------------
+
+# match-case
+note = [1,11,111]
+
+match note:
+    case [1,11,111]: # Du kannst auch Objekte übergeben
+        print("Mathe Genie")
+    case "1":
+        print("Sehr gut!")
+    case "2":
+        print("Gut!")
+    case "3":
+        print("Befriedigend.")
+    case "4":
+        print("Ausreichend.")
+    case "5":
+        print("Mangelhaft!")
+    case _:
+        print("Ungenügend!!!")
