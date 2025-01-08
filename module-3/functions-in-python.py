5e25569b11855622ddc1feac88f01ee25ba646e8
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2025-01-08 16:29:24 +0100

    while loop and infinite loop

--- module-3/functions-in-python.py
+++ module-3/functions-in-python.py
@@ -1,3 +1,4 @@
+# ----------------if-elif-else-Anweisungen-----------------------------------------
 # if-elif-else-Anweisungen
 note = input("Gib die Note ein: ")
 
@@ -14,8 +15,11 @@ elif note == "5":
 else:
     print("Ungenügend!!!")
 
-# --------------------------------------------------------------------------------
 
+
+
+
+# ----------------match-case-----------------------------------------
 # match-case
 note = [1,11,111]
 
@@ -35,12 +39,86 @@ match note:
     case _:
         print("Ungenügend!!!")
 
-# --------------------------------------------------------------------------------
 
-# for-Schleifen
+
+
+
+# ----------------for-Schleifen-----------------------------------------
+# for-Schleifen / Die Schleife durchläuft jedes Element (hier: jedes Passwort) in der Liste nacheinander,
+#  bis alle durchlaufen sind oder es durch ein break abgebrochen wird.
 passwörter = ["abc123", "gehe1m", "test", "123456"]
 
 for passwort in passwörter:
     print(passwort)
     if len(passwort) < 5:
         break
+
+
+
+
+
+# ----------------While-Schleifen-----------------------------------------
+# While-Schleifen / Läuft solange die Bedingung gegeben ist
+
+# while-Schleifen Beispiel-1
+x = 25
+runde = 1
+
+while x > 0:
+    print(f"Runde: {runde}")
+    runde += 1
+    x -=1
+
+# while-Schleifen Beispiel-2
+name = input("Gib einen Namen ein! X zum Abbruch: ")
+namensliste = []
+
+while name != "X":
+    namensliste.append(name)
+    print(namensliste)
+    name = input("Gib einen Namen ein! X zum Abbruch: ")
+
+
+
+
+
+# ----------------Endlosschleifen-----------------------------------------
+# Endlosschleifen
+# ACHTUNG hier wurde bewusst ein Break eingebaut, damit es nicht unendlich läuft
+
+teilnehmerliste = []
+
+teilnehmer = input("Gib einen Namen für die Endlosschleife ein!: ")
+i = 0
+
+while True:
+    teilnehmerliste.append(teilnehmer)
+    print(teilnehmerliste)
+    if len(teilnehmerliste) >= 1000:
+        break
+
+print(teilnehmerliste)
+
+
+
+# Es gibt aber Fälle, in denen eine Endlosschleife tatsächlich erwünscht ist. Das ist z. B. bei einem
+# Videospiel der Fall, das so lange laufen soll, bis der Spieler das Spiel beenden möchte. Hier
+# spricht man von einer sogenannten Hauptereignisschleife, die so lange ausgeführt wird, bis der
+# Benutzer das Programm beenden möchte. Als Beispiel schauen wir uns den folgenden
+# Ausschnitt aus einem Spiel an, das mit der Bibliothek pygame entwickelt wurde:
+
+# weiter = True
+# while weiter:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             weiter = False
+
+# In Zeile 1 wird mit weiter ein Schalter gesetzt, der dann kontinuierlich abgefragt wird. In
+# Zeile 3 werden alle Events, die im Spiel auftreten, abgefangen und nacheinander
+# durchgegangen. In Zeile 4 wird geprüft, ob das Event pygame.QUIT dabei war, d. h., der
+# Spieler möchte das Spiel beenden. Erst dann wird der Wert von weiter auf False gesetzt und
+# die Schleife dadurch beendet. Hier ergibt es Sinn, eine (zumindest temporäre) Endlosschleife
+# einzusetzen, da man ansonsten bspw. erraten müsste, wann der Spieler sein Spiel beenden
+# möchte. Eine weitere Anwendung von Endlosschleifen findet man bei der Client-/Server-
+# Programmierung. Der Server muss ständig laufen, um Anfragen von den Client-Programmen bei
+# Bedarf entgegennehmen zu können.
