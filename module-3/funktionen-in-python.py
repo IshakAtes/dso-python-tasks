99a89131e1c171312acbc9b516ce3a36723cc5ad
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2025-01-11 17:25:46 +0100

    added functions-in-python

---
+++ module-3/funktionen-in-python.py
@@ -0,0 +1,14 @@
+# ---------------- Functions -----------------------------------------
+# Beispiel-1
+def greet(name, lastname):
+    print(f"Hello {name} {lastname} !!!")
+
+greet("Ishak", "Feuer")
+
+# Beispiel-2
+def addieren(a, b):
+    summe = a + b
+    return summe
+
+s = addieren(11, 22)
+print(f"Summe ist: {s}")
