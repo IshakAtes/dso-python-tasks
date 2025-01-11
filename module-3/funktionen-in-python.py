7361f5cc0ac72fc5a3d680e0624d9ea159c61dc7
IshakAtes <108549234+IshakAtes@users.noreply.github.com>
2025-01-11 18:56:20 +0100

    args

--- module-3/funktionen-in-python.py
+++ module-3/funktionen-in-python.py
@@ -12,3 +12,34 @@ def addieren(a, b):
 
 s = addieren(11, 22)
 print(f"Summe ist: {s}")
+
+
+
+
+
+# ---------------- Args -----------------------------------------
+# Was sind nun Args. Das Beispiel ist, wie wir normalierweise eine addieren funktion schreiben würden. Sie ist aber nicht dynamisch genug
+def addieren(summand1, summand2, summand3):
+    summe = summand1 + summand2 + summand3
+    print(summe)
+
+addieren(1, 2, 3)
+
+# Wir schreiben jetzt einen Args Parameter, eine art Container oder Funktion die wir einmal definieren. Danach können wir sie mehrmals
+# und in verschiedenen szenarien verwenden,da wir nicht an die anzahl der parameter gebunden sind.
+def addieren(*summanden):
+    print(sum(summanden))
+
+addieren(99,-1,33)
+
+
+# Args Parameter kombinieren
+# Der positional Argument muss angegeben werden. Aber wenn ein stern dafür ist (*summanden), also wenn es ein args argument ist,
+# dann muss auch kein parameter angegeben werden.
+# Bemerkung: Es müssen immer als erstes die positional Arguments, also die normalen Variablen bzw parameter angegeben werden,
+# erst danach kommt der Args Parameter, da er so viele x beliebige parameter aufnahmen kann.
+def ausgabe(name, *summanden):
+    print(f"Hallo {name}")
+    print(f"Deine Zahlen: {summanden}")
+
+ausgabe("ishak", 45, 45, 55, 99, 33)
